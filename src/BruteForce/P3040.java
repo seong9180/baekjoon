3a6ccd5d23cd652016ba7fca989f2453d6b41aeb
seungmin <seong9180@naver.com>
2025-08-29 10:07:01 +0900

    feat: Boj 3040 브루트포스 풀이

---
+++ src/BruteForce/P3040.java
@@ -0,0 +1,60 @@
+/*
+    BOJ 3040 백설 공주와 일곱 난쟁이
+    Status: v1
+    Key:
+    Notes:
+    Last Reviewed: 2025-08-29
+*/
+
+package BruteForce;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P3040 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] gnome = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            gnome[i] = Integer.parseInt(st.nextToken());
+        }
+        //9명의 숫자 전부 합 구함
+        for (int i = 0; i < 9; i++) {
+            sum += gnome[i];
+        }
+        //System.out.println("sum = " + sum);
+
+        int diff = 0;
+        //7명의 합이 100이 되는 조건이 유일 -> 9명의 합에서 100을 빼면 스파이 두명의 합이 나옴.
+        diff = sum - 100;
+        int spy1 = 0, spy2 = 0;
+
+        outer:
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (gnome[i] + gnome[j] == diff) {
+                    spy1 = gnome[i];
+                    spy2 = gnome[j];
+                    break outer;
+                }
+            }
+        }
+//        System.out.println("spy1 = " + spy1);
+//        System.out.println("spy2 = " + spy2);
+
+        for (int i = 0; i < 9; i++) {
+            if(gnome[i] != spy1 && gnome[i] != spy2){
+                System.out.println(gnome[i]);
+            }
+        }
+
+
+
+    }
+
+
+}
