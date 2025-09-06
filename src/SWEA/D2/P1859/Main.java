0ec432930784e50e3de750f8cb4da24b9116a1d8
seungmin <seong9180@naver.com>
2025-09-06 12:36:20 +0900

    refact :

--- src/SWEA/D2/P1859/Main.java
+++ src/SWEA/D2/P1859/Main.java
@@ -1,6 +1,6 @@
 /*
     SWEA 1859 백만 장자 프로젝트
-    Status: v1
+    Status: v2 : input.txt 직접 테스트
     Key:
     Notes:
     Last Reviewed: 2025-09-05
@@ -12,10 +12,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //input.txt 파일을 직접 입력 스트림으로 연결
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("/Users/seungmin/Desktop/김승민/baekjoon/baekjoon/src/SWEA/D2/P1859/input.txt")));
 
         // TODO: solve
-        int t = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine()); ㄹㅍ
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
