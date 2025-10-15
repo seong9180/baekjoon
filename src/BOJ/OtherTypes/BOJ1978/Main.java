feae6531a296fffed6fd15edf691a92521b26c03
seungmin <seong9180@naver.com>
2025-10-15 23:56:01 +0900

    feat: 1978 풀이

---
+++ src/BOJ/OtherTypes/BOJ1978/Main.java
@@ -0,0 +1,34 @@
+/*
+    플랫폼 / 문제번호 / 문제이름
+    Status: v1
+    Key:
+    Notes:
+    Last Reviewed: 2025-00-00
+*/
+package BOJ.OtherTypes.BOJ1978;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (isPrime(num)) count++;
+        }
+        System.out.println(count);
+
+    }
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
