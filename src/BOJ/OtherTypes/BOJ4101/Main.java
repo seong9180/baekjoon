de865cf9387a6314158f39d4d378df2eccd0626c
seungmin <seong9180@naver.com>
2025-10-16 23:39:19 +0900

    feat: 4101 풀이

---
+++ src/BOJ/OtherTypes/BOJ4101/Main.java
@@ -0,0 +1,30 @@
+/*
+    플랫폼 / 문제번호 / 문제이름
+    Status: v1
+    Key:
+    Notes:
+    Last Reviewed: 2025-00-00
+*/
+package BOJ.OtherTypes.BOJ4101;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+//            String input = br.readLine();
+//            if(input == null) break; //eof
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) break; //문제 조건
+
+            System.out.println(a > b ? "Yes" : "No");
+        }
+    }
+}
