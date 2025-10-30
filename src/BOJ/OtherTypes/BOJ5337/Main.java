74f9b141ab6209d7b80c057c53bb532590661b24
seungmin <seong9180@naver.com>
2025-10-30 23:13:12 +0900

    feat: 5337

---
+++ src/BOJ/OtherTypes/BOJ5337/Main.java
@@ -0,0 +1,24 @@
+/*
+    플랫폼 / 문제번호 / 문제이름
+    Status: v1
+    Key:
+    Notes:
+    Last Reviewed: 2025-00-00
+*/
+package BOJ.OtherTypes.BOJ5337;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // TODO: solve
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+
+    }
+}
